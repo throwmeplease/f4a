8b048dd053524d9069fd4f2c06ec58f8a53cab96
evilcultist <66961093+EvilCultist@users.noreply.github.com>
2024-08-24 20:40:44 +0530

    bruhhh

--- taskhive/src/pages/index.js
+++ taskhive/src/pages/index.js
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <main
-            className={`bg-gradient-to-t from-purple-900 via-purple-800 to-black flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+            className={`bg-gradient-to-t from-purple-900 via-purple-800 to-black flex min-h-screen flex-col items-center justify-evenly p-24 ${inter.className}`}
         >
-            <Button className={'bg-gray-900 text-white'} variant="outline">login</Button>
+            <p className={'text-white text-9xl'}>TaskHive</p>
+            <Link href="/login">
+                <Button className={'bg-black text-white'} variant="outline">login</Button>
+            </Link>
         </main>
     );
 }
