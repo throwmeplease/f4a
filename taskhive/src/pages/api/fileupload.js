cc9417b36214fe3db2c6349ea94614c70ad41c69
Mauray-Jain <maurayjain@gmail.com>
2024-08-25 13:04:32 +0530

    Mai chalgaya

--- taskhive/src/pages/api/fileupload.js
+++ taskhive/src/pages/api/fileupload.js
@@ -3,20 +3,19 @@ import { readFileSync, rename } from "node:fs";
 
 export const config = {
 	api: {
-		bodyParser: {
-			sizeLimit: '13mb' // Set desired value here
-		}
+		bodyParser: false,
 	}
 }
 
 export default function handler(req, res) {
 	const name = readFileSync("cache")
 	var form = new formidable.IncomingForm();
+	form.uploadDir = `./repos/${name}`;
+	form.keepExtensions = true;
 	console.log("Main chalunga shayad")
 	form.parse(req, function (err, fields, files) {
-		var oldpath = files.filetoupload.filepath;
-		console.log(files.filetoupload.filepath);
-		var newpath = `./repos/${name}/` + files.filetoupload.originalFilename;
+		var oldpath = files.filetoupload[0].filepath;
+		var newpath = `./repos/${name}/` + files.filetoupload[0].originalFilename;
 		rename(oldpath, newpath, function (err) {
 			if (err) {
 				res.status(500).json({error: "Couldn't move"});
